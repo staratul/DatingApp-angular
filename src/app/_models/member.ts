c9ec009a05124bde60c63ba93bea003a54948630
staratul <atulji.star@gmail.com>
2025-01-24 15:58:40 +0530

    client chagnes

---
+++ src/app/_models/member.ts
@@ -0,0 +1,20 @@
+import { Photo } from "./photo"
+
+export interface Member {
+  id: number
+  userName: string
+  age: number
+  photoUrl: string
+  knownAs: string
+  created: Date
+  lastActive: string
+  gender: string
+  introduction: string
+  interests: string
+  lookingFor: string
+  city: string
+  country: string
+  photos: Photo[]
+}
+
+  
